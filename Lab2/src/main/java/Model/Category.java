1ea55f213581e27407b491385ce9162e6d2d9746
dumi <lenca_1995@mail.ru>
2018-03-07 00:47:18 +0200

    category class

--- Lab2/src/main/java/Model/Category.java
+++ Lab2/src/main/java/Model/Category.java
@@ -12,22 +12,6 @@ public class Category {
         this.name = name;
         this.categoryId = categoryId;
     }
-
-    public static Category fromCSV(String csv, String delimiter) {
-        String[] dates = csv.split(delimiter);
-        int id = Integer.parseInt(dates[0]);
-        String name = dates[1];
-        int categoryId = dates.length == 2 ? null : Integer.parseInt(dates[2]);
-        return new Category(id, name, categoryId);
-    }
-
-    @Override public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", categoryId=" + categoryId +
-                ", total=" + total +
-                '}';
-    }
+    
 
 }
